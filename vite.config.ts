e5bc93b78b39d9c40528136ed71ea80f009ca5ef
David Alves <david_castor3@hotmail.com>
2022-04-29 10:39:27 +0200

    build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
+  base: '/dialog-editor/',
   resolve: {
     alias: {
       '~': resolve('src'),
