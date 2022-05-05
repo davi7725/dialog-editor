b740df6e4723c3ed2e66f66e18445534b219bd83
David Alves <david_castor3@hotmail.com>
2022-05-05 12:03:30 +0200

    Updated gitignore. Updated deployer script

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:davi7725/dialog-editor.git main:gh-pages
+git push -f git@github.com:davi7725/dialog-editor.git master:gh-pages
 
 cd -
